9596c0666441b6cea03f4b38f91fa56b062cf04b
William Elí O.L <ramun346416@gmail.com>
2022-06-23 20:57:39 -0500

    Codigo prueba secuancia leds cubo

---
+++ cubo_3x3x3x3.ino
@@ -0,0 +1,29 @@
+int Columnas[] = {5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+ 
+int Filas[] = {2, 3, 4};
+int RandFila;     //Variable para fila aleatoria
+int RandColumna;  //Variable para columna aleatoria
+ 
+void setup()
+{
+  int contador;
+
+  for (int contador = 4; contador < 17; contador++){
+    pinMode(Columnas[contador], OUTPUT); }
+ 
+  for (int contador = 2; contador < 4; contador++){
+    pinMode(Filas[contador], OUTPUT); }
+}
+ 
+void loop()
+
+{for (int contador1 = 2; contador1 < 4;contador1++){
+  digitalWrite (Filas, HIGH);
+  for (int contador2 = 4; contador2 < 17; contador2++){
+   digitalWrite (Columnas, HIGH);
+   delay (300);
+  digitalWrite (Columnas, LOW);}
+  digitalWrite (Filas, LOW);}
+  
+
+}
