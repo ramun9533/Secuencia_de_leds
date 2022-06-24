680f9c0dbfad56e2760a3d4b0e6e56cf98ccbf65
William Elí O.L <ramun346416@gmail.com>
2022-06-23 21:14:23 -0500

    Update cubo_3x3x3x3.ino

--- cubo_3x3x3x3.ino
+++ cubo_3x3x3x3.ino
@@ -12,17 +12,19 @@ void setup()
     pinMode(Columnas[contador], OUTPUT); }
  
   for (int contador = 2; contador < 4; contador++){
-    pinMode(Filas[contador], OUTPUT); }
-    digitalWrite (2, HIGH);
+    pinMode(Filas[contador], OUTPUT); } 
+ 
+    
 }
  
-void loop(){
+void loop(){digitalWrite (2, HIGH);
 
 //for (int contador1 = 2; contador1 < 4;contador1++){
  // digitalWrite (Filas, HIGH);
-for (int contador2 = 4; contador2 < 17; contador2++){
+for (int contador2 = 5; contador2 < 17; contador2++){
    digitalWrite (Columnas, HIGH);
    delay (300);
-  digitalWrite (Columnas, LOW);}
+ // digitalWrite (Columnas, LOW);
+ }
   }
   
