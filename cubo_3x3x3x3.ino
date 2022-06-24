b326602b4aa83b745467a41bd2b5a4a9b56df9ef
William Elí O.L <ramun346416@gmail.com>
2022-06-23 21:30:38 -0500

    Update cubo_3x3x3x3.ino

--- cubo_3x3x3x3.ino
+++ cubo_3x3x3x3.ino
@@ -1,24 +1,31 @@
 int Columnas[] = {5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
  
 int Filas[] = {2, 3, 4};
-int RandFila;     //Variable para fila aleatoria
-int RandColumna;  //Variable para columna aleatoria
+//int RandFila;     //Variable para fila aleatoria
+//int RandColumna;  //Variable para columna aleatoria
  
 void setup()
 {
   int contador;
 
-  for (int contador = 4; contador < 17; contador++){
+  for (int contador = 5; contador < 17; contador++){
     pinMode(Columnas[contador], OUTPUT); }
  
   for (int contador = 2; contador < 4; contador++){
-    pinMode(Filas[contador], OUTPUT); } 
- 
+    pinMode(Filas[contador], OUTPUT); }
     
+
 }
  
-void loop(){digitalWrite (2, HIGH);
-digitalWrite (5, HIGH);
+void loop(){
+
+for (int contador = 2; contador < 17; contador++){
+  digitalWrite (Filas [contador], LOW); 
+  
+  
+}
+digitalWrite (17, HIGH);
+digitalWrite (2, HIGH);
 //for (int contador1 = 2; contador1 < 4;contador1++){
  // digitalWrite (Filas, HIGH);
 //for (int contador2 = 5; contador2 < 17; contador2++){
@@ -27,3 +34,5 @@ digitalWrite (5, HIGH);
  // digitalWrite (Columnas, LOW);
  //}
   }
+  
+
